cea3bfd688a26a7c5fc3044bd5f4064c9c239d58
AntonChetveryk <chetverykanton92@gmail.com>
2020-03-15 16:12:20 +0200

    refactor renderPlanets

--- src/renderPlanets.js
+++ src/renderPlanets.js
@@ -3,9 +3,9 @@ import planetImg from "./img/planet.jpg";
 
 async function renderPlanets(currentPage) {
   const container = document.getElementById("container-planets");
-  const persons = await getInfo("planets", { page: currentPage });
+  const planets = await getInfo("planets", { page: currentPage });
   container.innerHTML = "";
-  persons.forEach(planet => {
+  planets.forEach(planet => {
     const div = document.createElement("div");
     div.className = "planets";
     div.innerHTML = `
